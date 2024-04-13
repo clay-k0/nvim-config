8b537053ac08d82e4975a7c6cd6429afd1cacea5
Clayton King <ClaytonKing03@gmail.com>
2024-04-13 02:19:52 -0400

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,12 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.gruvbox_material_background = "medium"
-vim.g.gruvbox_material_foreground = "mix"
+vim.g.gruvbox_material_background = "soft"
+vim.g.gruvbox_material_foreground = "original"
 
 -- vim.g.gruvbox_material_background = "hard"
 -- vim.g.gruvbox_material_foreground = "original"
-
-vim.g.gruvbox_transparent_background = 2
 
 vim.opt.wrap = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,23 +5,12 @@ return {
       transparent_background = 1,
     },
   },
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      -- transparent_mode = true,
-    },
-  },
-  { "AlexvZyl/nordic.nvim" },
-  {
-    "ricardoraposo/gruvbox-minor.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  { "morhetz/gruvbox.nvim" },
+  { "maralla/gomod.vim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-minor",
+      colorscheme = "gruvbox-material",
     },
   },
 }
