2314609a308add4015cbfa0e6d2036fcac0ace12
Clayton King <ClaytonKing03@gmail.com>
2024-04-15 18:13:22 -0400

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,18 +5,20 @@ return {
       transparent_background = 1,
     },
   },
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard",
+      palette_overrides = {
+        gray = "#2ea542",
+      },
+    },
+  },
   { "maralla/gomod.vim" },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-material",
-      opts = {
-        contrast = "hard",
-        palette_overrides = {
-          gray = "#2ea542",
-        },
-      },
     },
   },
 }
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,3 @@
 return {
   { "folke/flash.nvim", enabled = false },
-  { "zbirenbaum/copilot-cmp", enabled = false },
 }
